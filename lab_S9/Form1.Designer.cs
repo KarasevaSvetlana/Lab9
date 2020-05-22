03079a8549f5e258f0cecc18c98cb0a654c74aaf
Светлана Краснокутская <krasnokutskaya.sa@edu.spbstu.ru>
2020-05-22 12:52:36 +0300

    Добавьте файлы проекта.

---
+++ lab_S9/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace lab_S9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AllowDrop = true;
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.MinimumSize = new System.Drawing.Size(480, 400);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(1);
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 130F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 556);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(24, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(752, 398);
+            this.panel1.TabIndex = 3;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34.38596F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65.61404F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 97F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 165F));
+            this.tableLayoutPanel2.Controls.Add(this.trackBar2, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button2, 3, 1);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.trackBar1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 2, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(24, 428);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(752, 124);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(589, 3);
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(157, 45);
+            this.trackBar2.TabIndex = 3;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(589, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(171, 3);
+            this.trackBar1.Maximum = 5;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(165, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(492, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Скорость";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Кол-во";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(492, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 556);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        public System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ lab_S9/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_S9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            speedButton = trackBar2.Value;
+            countButton = trackBar1.Value;
+            label4.Text = trackBar2.Value.ToString();
+            label3.Text = trackBar1.Value.ToString();
+        }
+        int speedButton = 1;
+        int countButton = 1;
+        int i = 1;
+        List<Thread> threads = new List<Thread>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < countButton; i++)
+            {
+
+                Thread thread = new Thread(() =>
+                {
+
+                    Invoke((MethodInvoker)(() =>
+                    {
+
+                        Button myButton = new Button();
+                        myButton.Name = "button_" + i;
+                        myButton.Location = new Point(0 + i * 30, 0 + i * 30);
+                        myButton.Size = new Size(152, 32);
+                        myButton.TabIndex = 1;
+                        i = i + 1;
+                        myButton.Text = "поймай меня";
+                        myButton.Click += new EventHandler(button_Click_win);
+                        myButton.MouseMove += new MouseEventHandler(button1_MouseMove);
+                        panel1.Controls.Add(myButton);
+                        panel1.Update();
+                        Thread.Sleep(500);
+
+                    }));
+
+                });
+                thread.Start();
+                threads.Add(thread);
+
+            }
+        }
+        private void button_Click_win(object sender, EventArgs e)
+        {
+            MessageBox.Show("YOU ARE WINNER!!!");
+        }
+            private void button2_Click(object sender, EventArgs e)
+        {
+            int count = Controls.Count;
+            for (int i = 0; i < threads.Count; i++)
+            {
+                threads[i].Abort();
+
+            }
+            threads.Clear();
+            panel1.Controls.Clear();
+
+            i = 1;
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            Button b = (Button)sender;
+            Point cur = this.PointToClient(Cursor.Position);
+            Point loc = b.Location;
+            int x_new = loc.X;
+            int y_new = loc.Y;
+
+            //Движение вправо
+            if (cur.X < loc.X + b.Width)
+                x_new += speedButton * 20;
+
+            //Движение влево
+            if (cur.X > loc.X + b.Width)
+                x_new -= speedButton * 20;
+
+            //Движение вверх
+            if (cur.Y > loc.Y + b.Height)
+                y_new -= speedButton * 20;
+
+            //Движение вниз
+            if (cur.Y < loc.Y + b.Height)
+                y_new += speedButton * 20;
+
+            //Отступ слева
+            if (x_new < 5)
+                x_new = 5;
+
+            //Отступ справа
+            if (x_new > panel1.Width - b.Width - 25)
+                x_new = panel1.Width - b.Width - 25;
+
+            //Отступ сверху
+            if (y_new < 10)
+                y_new = 10;
+
+            //Отступ снизу
+            if (y_new > panel1.Height - b.Height - 45)
+                y_new = panel1.Height - b.Height - 45;
+            //курсор внизу формы
+            if (y_new > panel1.Height - b.Height - 50)
+                y_new = panel1.Height - b.Height - 80;
+
+            //курсор вверху формы
+            if (y_new < 11)
+                y_new += 50;
+
+            //курсор справа формы
+            if (x_new > panel1.Width - b.Width - 26)
+                x_new += -100;
+
+            //курсор слева формы
+            if (x_new < 6)
+                x_new += 100;
+
+
+
+            b.Location = new Point(x_new, y_new);
+            panel1.Update();
+            //Thread.Sleep(500);
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            speedButton = trackBar2.Value;
+            countButton = trackBar1.Value;
+            label4.Text = trackBar2.Value.ToString();
+            label3.Text = trackBar1.Value.ToString();
+        }
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+
+            speedButton = trackBar2.Value;
+            countButton = trackBar1.Value;
+            label4.Text = trackBar2.Value.ToString();
+            label3.Text = trackBar1.Value.ToString();
+        }
+    }
+}
